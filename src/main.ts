ee040e89fbd101ee6ff2634e7855878c9eb0af5b
odlopez16 <lopezosbel02@gmail.com>
2024-10-01 13:00:48 -0400

    solucionando los problemas de prettier y CORS

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log(`Application is running on url: ${await app.getUrl()}`);
+
+  app.enableCors()
 }
 bootstrap();
